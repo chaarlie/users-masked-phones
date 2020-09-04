b5e4d44d28cb738919cf86fdb4e56d0318b45551
Carlos De la Rosa <cdelarosa@sweatworks.net>
2020-09-04 19:29:09 -0400

    adding caching

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const Koa = require('koa');
 const Router = require('koa-router');
+const NodeCache = require( "node-cache" );
 
-const { users: { find} } = require('./db/')
+const { users: {find} } = require('./db/');
+const phoneMasking = require('./util/phoneMasking');
 
 const app = new Koa();
 const router = new Router({prefix: '/api/v1'});
+const caching =  new NodeCache( { stdTTL: 60 * 60 * 24, checkperiod: 60 * 60 * 24} );
 
 // eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 3000;
@@ -13,14 +16,25 @@ const HOST = process.env.HOST || 'localhost';
 
 router.get('/users', async (ctx) => {
   const masking = ctx.request.query.masking || '';
-  
+  let users = null;
+
   if(masking) {
-    const users = await find();
-    
-    ctx.body = users
+    users = caching.get('users-masked');
+    if(!users) {
+      users = await find();
+      users = users.map((u) => Object.assign({}, {...u, phone: phoneMasking(u.phone)}));
+      caching.set('users-masked', users);
+    }
   }
-  else 
-    ctx.body = 'hello world';
+  else {
+    users = caching.get('users');
+    if(!users) {
+      users = await find();
+      caching.set('users', users);
+    }
+  }
+
+  ctx.body = users;
 });
 
 
--- util/phoneMasking.js
+++ util/phoneMasking.js
@@ -1,11 +1,11 @@
-module.exports = phone => {
+module.exports = (phone, mask = '*****') => {
   const pattern =/\d{10}/g;
   const strippedPhone = phone.match(pattern);
   let parsedPhone = null;
 
   if(strippedPhone ) { //is a phone number 
     parsedPhone  = strippedPhone[0];
-    parsedPhone = `+1${parsedPhone.substr(6)}`;
+    parsedPhone = `+1${mask}${parsedPhone.substr(6)}`;
   }
   return parsedPhone;
 }
